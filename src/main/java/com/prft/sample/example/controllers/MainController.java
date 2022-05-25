d0d4e281227790ac29017355419a9c90d757c25a
Charles.Mahoney <charles.mahoney@PRFT-13533-MAC.local>
2022-05-25 15:27:17 -0400

    add mock functionality

--- src/main/java/com/prft/sample/example/controllers/MainController.java
+++ src/main/java/com/prft/sample/example/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.prft.sample.example.controllers;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,11 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prft.sample.example.services.SimpleService;
+import com.prft.sample.example.utils.Utils;
 
 import net.logstash.logback.marker.Markers;
 
@@ -23,6 +27,8 @@ public class MainController {
 	@Autowired
 	SimpleService simpleService;
 	
+	@Autowired
+	Utils utils;
 	
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String getbackend() {
@@ -44,5 +50,21 @@ public class MainController {
 		
 		return "Success Updated 0740";
 		
+	}
+	
+	@RequestMapping(value="/**/",method=RequestMethod.GET)
+	public ResponseEntity<String> MainResponse(HttpServletRequest request){
+	    String uri = request.getRequestURI();
+		
+		String returnStr = null;
+		try {
+			returnStr = utils.getFile(uri);
+		} catch (IOException e) {
+			LOGGER.error("Error finding file for URI: " + uri + " ErrorMessage: " + e.getMessage());
+			return new ResponseEntity<String>(e.getMessage(),HttpStatus.NOT_FOUND);
+		}
+		
+		LOGGER.info("Returning for URI: "+uri);
+		return new ResponseEntity<String>(returnStr,HttpStatus.OK);
 	}
 }
